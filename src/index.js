0017c667a74b497c9ad381daf510e980045d121a
mpallor <maria.pallor@gmail.com>
2022-08-10 16:33:45 +0200

    Created function displayCurrentDate in js

--- src/index.js
+++ src/index.js
@@ -12,3 +12,49 @@ let apiKey = "c6250be52ad7bd4f277e12656c8e390e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cracow&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+function displayCurrentDate() {
+  let date = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "Jan.",
+    "Feb.",
+    "Mar.",
+    "Apr.",
+    "May",
+    "June",
+    "July",
+    "Aug.",
+    "Sept.",
+    "Oct.",
+    "Nov.",
+    "Dec.",
+  ];
+  let day = days[date.getDay()];
+  let todayDate = date.getDate();
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  let month = months[date.getMonth()];
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  let displayDate = document.querySelector("#current-date");
+  displayDate.innerHTML = `${hour}:${minute} ${day}, ${todayDate} ${month}`;
+}
+displayCurrentDate();
